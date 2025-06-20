da1853a8853a06592c84797023d5751a807b5d75
robbenselva <robbenselva@gmail.com>
2025-06-20 13:58:42 +0530

    Update app.js
    a

--- src/app.js
+++ src/app.js
@@ -1,1 +1,11 @@
-sca
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (_req, res) => {
+  res.send('Hello from AWS CodePipeline!');
+});
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
